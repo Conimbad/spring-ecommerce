7b904e5261a79170a39e1a1def4021e5a22e02dd
conimbad <escocarl@gmail.com>
2024-04-29 15:52:56 -0600

    Se agregó la funcionalidad de mostrar productos para el Administrador

--- src/main/java/com/ecommerce/springecommerce/service/ProductoService.java
+++ src/main/java/com/ecommerce/springecommerce/service/ProductoService.java
@@ -1,5 +1,6 @@
 package com.ecommerce.springecommerce.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import com.ecommerce.springecommerce.model.Producto;
@@ -10,5 +11,5 @@ public interface ProductoService {
   public Optional<Producto> get(Integer id);
   public void update(Producto producto);
   public void delete(Integer id);
-  
+  public List<Producto> findAll();
 }
