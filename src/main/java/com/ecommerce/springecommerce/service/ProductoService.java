1dd73005dfc44fd75d8e4082ee597a0189999104
conimbad <escocarl@gmail.com>
2024-04-29 09:48:40 -0600

    Creación de vista de productos y Controller productos

--- src/main/java/com/ecommerce/springecommerce/service/ProductoService.java
+++ src/main/java/com/ecommerce/springecommerce/service/ProductoService.java
@@ -1,5 +1,14 @@
 package com.ecommerce.springecommerce.service;
 
+import java.util.Optional;
+
+import com.ecommerce.springecommerce.model.Producto;
+
 public interface ProductoService {
 
+  public Producto save(Producto producto);
+  public Optional<Producto> get(Integer id);
+  public void update(Producto producto);
+  public void delete(Integer id);
+  
 }
--- src/main/java/com/ecommerce/springecommerce/service/ProductoServiceImpl.java
+++ src/main/java/com/ecommerce/springecommerce/service/ProductoServiceImpl.java
@@ -1,5 +1,41 @@
 package com.ecommerce.springecommerce.service;
 
-public class ProductoServiceImpl {
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.springecommerce.model.Producto;
+import com.ecommerce.springecommerce.repository.ProductoRepository;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+  @Autowired
+  private ProductoRepository productoRepository;
+
+  @Override
+  public Producto save(Producto producto) {
+    // TODO Auto-generated method stub
+    return productoRepository.save(producto);
+  }
+
+  @Override
+  public Optional<Producto> get(Integer id) {
+    // TODO Auto-generated method stub
+    return productoRepository.findById(id);
+  }
+
+  @Override
+  public void update(Producto producto) {
+    // TODO Auto-generated method stub
+    productoRepository.save(producto);
+  }
+
+  @Override
+  public void delete(Integer id) {
+    // TODO Auto-generated method stub
+    productoRepository.deleteById(id);
+  }
 
 }
