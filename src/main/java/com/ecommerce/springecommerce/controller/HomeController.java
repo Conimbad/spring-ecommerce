99c034b995156cc7554979ef27e0ac99c29073f6
conimbad <escocarl@gmail.com>
2024-05-07 12:31:50 -0600

    Creamos la vista de carrito de compras, modificamos el botón de productohome 'añadir al carrito'

--- src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
+++ src/main/java/com/ecommerce/springecommerce/controller/HomeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommerce.springecommerce.model.Producto;
@@ -40,5 +41,10 @@ public class HomeController {
 
     log.info("Id de producto enviado como parámetro {}", id);
     return "usuario/productohome";
+  }
+
+  @PostMapping("cart")
+  public String addCart() {
+    return "usuario/carrito";
   }
 }
