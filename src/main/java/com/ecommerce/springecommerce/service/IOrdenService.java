80d46730d875533f5007474bb1ed41accef6a4d8
conimbad <escocarl@gmail.com>
2024-06-28 15:45:59 -0600

    Agregamos la funcionalidad ver detalle de compra al usuario

--- src/main/java/com/ecommerce/springecommerce/service/IOrdenService.java
+++ src/main/java/com/ecommerce/springecommerce/service/IOrdenService.java
@@ -2,6 +2,7 @@ package com.ecommerce.springecommerce.service;
 
 
 import java.util.List;
+import java.util.Optional;
 
 import com.ecommerce.springecommerce.model.Orden;
 import com.ecommerce.springecommerce.model.Usuario;
@@ -9,6 +10,8 @@ import com.ecommerce.springecommerce.model.Usuario;
 public interface IOrdenService {
 
     List<Orden> findAll();
+
+    Optional<Orden> findById (Integer id);
 
     Orden save (Orden orden);
     String generarNumeroOrden();
